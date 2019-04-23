c3b1986e82389e46ac1e487f64c6fbb7ff65b2c8
Ilham Rizki Hidayat <ilkuntwentyfive@gmail.com>
2019-04-24 05:15:48 +0700

    UPDATE LAGI
    add printParent

--- flight.cpp
+++ flight.cpp
@@ -1,5 +1,23 @@
 #include "flight.h"
 /// add later just in case we wanna change it
+void printParent (ListParent L)
+{
+    if (firstParent(L) != NULL)
+    {
+        adrParent P = firstParent(L);
+        while (P != NULL)
+        {
+            cout << tujuan(P) << " - ";
+            P = nextParent(P);
+        }
+        cout << "done" << endl;
+    }
+    else
+    {
+        cout << "nope" << endl;
+    }
+}
+
 void printChild(ListChild L)
 {
     if (firstChild(L) != NULL)
--- main.cpp
+++ main.cpp
@@ -8,22 +8,34 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Welcome to Bojongsoang International Airport!" << endl;
 
     ListChild L1;
     createListCh(L1);
     insertFirstCh(L1, allocateCh("Singapore Airlines","3"));
     insertFirstCh(L1, allocateCh("Garuda Indonesia","9"));
-    adrChild Prec = firstChild(L1);
-    //cout << maskapai(Prec);
-    insertAfterCh(L1, Prec, allocateCh("KLM","88"));
+    insertAfterCh(L1, searchChild(L1,"3"), allocateCh("KLM","88"));
     insertLastCh(L1, allocateCh("Emirates","77"));
     printChild(L1);
     adrChild C;
     deleteFirstCh(L1,C);
     deleteLastCh(L1,C);
-    Prec = firstChild(L1);
+    adrChild Prec = searchChild(L1,"3");
     deleteAfterCh(L1, Prec, C);
     printChild(L1);
+    cout << endl;
+    ListParent L2;
+    createListPar(L2);
+    insertFirstPar(L2, allocatePar("Tokyo","22"));
+    insertFirstPar(L2, allocatePar("Jakarta","45"));
+    insertAfterPar(L2, searchParent(L2,"22"), allocatePar("London","99"));
+    insertLastPar(L2, allocatePar("Bali","100"));
+    printParent(L2);
+    adrParent P;
+    deleteFirstPar(L2,P);
+    deleteLastPar(L2,P);
+    adrParent PrecP = searchParent(L2,"22");
+    deleteAfterPar(L2,PrecP,P);
+    printParent(L2);
     return 0;
 }
